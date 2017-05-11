5ae69ce4476f8565ebfd4d5a375046b37b8afc4d
Zack <cookies951@gmail.com>
2017-05-11 10:14:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -422,7 +422,7 @@ var c = document.getElementById('canvas');
 
             function regenerate(){
                 if(cool == 50){
-                    if(Otime % 3 == 0) power-=1;
+                    if(Otime % 3 == 0 && power > 0) power-=1;
                     if(health < 100) health++;
                     if(Otime % 2 == 0){
                         wx+=5;
@@ -1050,7 +1050,7 @@ var c = document.getElementById('canvas');
 
                     drawChar();
                     
-                    if(regeneration && damaging == false && power >=1) regenerate();
+                    if(regeneration && damaging == false) regenerate();
 
                     stateDef(enemy1);
 
