a5c2bdf8eb90dd5dccf1af54cf256fb6be857256
Zachary Traczyk <cookies951@gmail.com>
2022-08-27 19:18:02 -0500

    change shop enemyBody to actual enemyBody

--- src/game/shop/bodies/bodyEnemy.ts
+++ src/game/shop/bodies/bodyEnemy.ts
@@ -9,13 +9,45 @@ const bodyEnemy: BodyFunction = (
   const colorCool = Body.enemy.colorCool;
   const colorDamg = Body.enemy.colorDamg;
   // Translate center x,y to draw corner
+  w /= 2;
+
   x -= w / 2;
   y -= h / 2;
 
   ctx.fillStyle = cool > 0 ? colorCool : colorNorm;
   if (damaging) ctx.fillStyle = colorDamg;
 
-  ctx.fillRect(x, y, w, h);
+  ctx.beginPath();
+
+  ctx.strokeStyle = "black";
+  //Draws Body
+  ctx.moveTo(x - w / 8, y);
+  ctx.bezierCurveTo(
+    x - w / 8,
+    y - h / 4,
+    x + w + w / 8,
+    y - h / 4,
+    x + w + w / 8,
+    y
+  );
+
+  ctx.bezierCurveTo(x + w * 2, y, x + w * 2, y + h, x + w - w / 8, y + h);
+
+  ctx.bezierCurveTo(
+    x + w - w / 8,
+    y + h * 1.75,
+    x - w * 2,
+    y + h / 4,
+    x - 10,
+    y + h / 2 - 10
+  );
+
+  ctx.bezierCurveTo(x - w - 5, y + h / 2, x - w / 4, y, x - w / 8, y);
+  ctx.fill();
+  // ctx.stroke();
+  ctx.closePath();
+
+  ctx.lineWidth = 3;
 };
 
 export default bodyEnemy;
