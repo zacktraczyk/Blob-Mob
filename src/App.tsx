76434e812b3247faa5540f66855880698513f96a
Zack Traczyk <me@zacktraczyk.com>
2023-03-28 02:24:15 -0700

    fix: simplify resize warn tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ const ResizeScreenWarning: React.FC = () => {
   game.paused = true
 
   return (
-    <div className='fixed top-0 left-0 right-0 bottom-0 z-50 flex flex-col items-center justify-center overflow-hidden bg-main'>
+    <div className='fixed inset-0 z-50 flex flex-col items-center justify-center overflow-hidden bg-main'>
       <h1 className='text-center text-2xl font-bold text-text-alt'>Screen Size Not Supported</h1>
       <div className='h-5'></div>
       <h1 className='text-center text-xl italic'>Please Resize the Screen</h1>
